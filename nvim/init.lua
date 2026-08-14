c182f7b1b6061598e0dd06c890e98d6b6f77a430
Jack Heckenlaible <176347710+jacktheck02@users.noreply.github.com>
2026-08-14 09:35:31 -0700

    Refactor telescope keymaps

--- nvim/init.lua
+++ nvim/init.lua
@@ -414,11 +414,17 @@ do
     -- You can put your default mappings / updates / etc. in here
     --  All the info you're looking for is in `:help telescope.setup()`
     --
-    -- defaults = {
-    --   mappings = {
-    --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-    --   },
-    -- },
+    defaults = {
+      mappings = {
+        -- i = { ['<c-enter>'] = 'to_fuzzy_refine',},
+        n = {
+          ['t'] = 'select_tab',
+          ['v'] = 'select_vertical',
+          ['x'] = 'select_horizontal',
+          ['d'] = 'select_drop',
+        },
+      },
+    },
     -- pickers = {}
     extensions = {
       ['ui-select'] = { require('telescope.themes').get_dropdown() },
